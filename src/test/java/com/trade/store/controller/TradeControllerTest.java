0261c7d1e4e4fe0b439aab01712deee0aca32f8a
shashanksinghin <shashanksingh2005@gmail.com>
2025-06-22 21:41:39 +0530

    corrected failing unit test

--- src/test/java/com/trade/store/controller/TradeControllerTest.java
+++ src/test/java/com/trade/store/controller/TradeControllerTest.java
@@ -1,6 +1,6 @@
 package com.trade.store.controller;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,12 +11,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.trade.store.kafka.TradeProducer;
 import com.trade.store.model.Trade;
 
 import lombok.NoArgsConstructor;
@@ -40,7 +40,7 @@ class TradeControllerTest {
 	 * 
 	 */
 	@MockitoBean
-	private KafkaTemplate<String, Trade> kafkaTemplate;
+	private TradeProducer tradeProducer;
 
 	/**
 	 * 
@@ -50,7 +50,7 @@ class TradeControllerTest {
 	void testSendTrade() throws Exception {
 
 		final Trade trade = new Trade("T1", 1, "CP-1", "B1", LocalDate.now().plusDays(10), LocalDate.now(), false);
-		when(kafkaTemplate.send("trade", trade)).thenReturn(null);
+		doNothing().when(tradeProducer).sendTrade(trade);
 
 		final ObjectMapper objectMapper = new ObjectMapper();
 		objectMapper.registerModule(new JavaTimeModule());
